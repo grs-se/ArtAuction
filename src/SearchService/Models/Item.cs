84d78ebb8204d324fe07a640f85b1bf2b324f222
grs-se <george_ricesmith@hotmail.com>
2023-12-20 17:16:29 +0000

    Create Search Microservice

---
+++ src/SearchService/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Models
+{
+    public class Item : Entity
+    {
+        public int ReservePrice { get; set; }
+        public string Seller { get; set; }
+        public string Winner { get; set; }
+        public int SoldAmount { get; set; }
+        public int CurrentHighBid { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime AuctionEnd { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Medium { get; set; }
+        public string Category { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
